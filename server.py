61bec76f442323def161a33c185dd8660c1ed7da
Infidel <Vud150294@gmail.com>
2023-08-30 22:06:04 +0500

    Удалил не нужную функцию

--- server.py
+++ server.py
@@ -5,23 +5,20 @@ import itertools
 
 
 async def websocket_handler(request):
-
+    print('Websocket connection starting')
     ws = web.WebSocketResponse()
     await ws.prepare(request)
-
-    incoming = asyncio.ensure_future(process_incoming_messages(ws))
 
     for i in itertools.count():
         if ws.closed:
             break
-        await asyncio.sleep(4)
+        print('Websocket connection ready')
         flashlight_control_commands = [{b'\x12\x00\x00'}, {b' \x00\x03\xff\xff\xf4'},
                                        {b' \x00\x03\0x91\0x79\0xe7'}, {b'\x13\x00\x00'}]
 
         for command in flashlight_control_commands:
-            await asyncio.sleep(4)
-            for j in command:
-                byte_number = j
+            await asyncio.sleep(2)
+            for byte_number in command:
                 print(f"sending: {byte_number}")
                 await ws.send_bytes(byte_number)
                 print(f"sent: {byte_number}")
@@ -29,15 +26,7 @@ async def websocket_handler(request):
         await ws.close()
         break
     print('websocket connection closed')
-
-    await incoming
-
     return ws
-
-
-async def process_incoming_messages(ws):
-    async for msg in ws:
-        pass
 
 
 def main():
