368332829d0b52d7594d4264d58b2aaadc4490db
Infidel <Vud150294@gmail.com>
2023-09-01 15:54:26 +0500

    Изменил название переменных, добавил документацию по работе функции

--- encode_tlv.py
+++ encode_tlv.py
@@ -2,21 +2,26 @@ from struct import *
 
 
 def parse_tlv(data):
+    """
+    function for encoding from tlv format
+    :param data: tlv array
+    :return: encode tlv
+    """
     index = 0
     while index < len(data):
-        t = data[index]
+        type_tlv = data[index]
         index += 1
 
-        l = int.from_bytes(data[index:index + 2], byteorder='big')
+        length = int.from_bytes(data[index:index + 2], byteorder='big')
         index += 2
 
-        if len(data[index:index + l]) == 3:
-            v = unpack('>BBB', data[index:index + l])
-            index += l
-            return {'type': t, 'length': l, 'value': v}
+        if len(data[index:index + length]) == 3:
+            value = unpack('>BBB', data[index:index + length])
+            index += length
+            return {'type': type_tlv, 'length': length, 'value': value}
         else:
-            index += l
-            return {'type': t, 'length': l}
+            index += length
+            return {'type': type_tlv, 'length': length}
 
 
 def lamp_control_knob(s):
