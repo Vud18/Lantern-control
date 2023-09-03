4c5ae1c391e413f69340a8f62ae0acb8a1f06e04
Infidel <Vud150294@gmail.com>
2023-09-03 18:08:31 +0500

    t

--- server.py
+++ server.py
@@ -8,14 +8,12 @@ async def websocket_handler(request):
     ws = web.WebSocketResponse()
     await ws.prepare(request)
 
-    print('Websocket connection ready')
     flashlight_control_commands = [{b'\x12\x00\x00'}, {b' \x00\x03\xff\xff\xf4'},
                                     {b' \x00\x03\0x91\0x79\0xe7'}, {b'\x13\x00\x00'}]
 
     for command in flashlight_control_commands:
         await asyncio.sleep(2)
         for byte_number in command:
-            print(f"sending: {byte_number}")
             await ws.send_bytes(byte_number)
             print(f"sent: {byte_number}")
 
